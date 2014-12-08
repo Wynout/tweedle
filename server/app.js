b0c6db31089adb5901ae00cd66d30f1e8033c560
Wynout van der Veer <wynout@gmail.com>
2014-12-08 16:53:10 +0100

    Refactor server/app.js

--- server/app.js
+++ server/app.js
@@ -1,15 +1,39 @@
-var config      = require('./../config.json'),
-    app         = require('express')(),
-    server      = require('http').createServer(app),
-    io          = require('socket.io')(server),
+/*jshint node:true*/
+'use strict';
+
+var compress    = require('compression'),
+    config      = require('./../config.json'),
     express     = require('express'),
+    app         = require('express')(),
+    favicon     = require('serve-favicon'),
+    http        = require('http'),
+    server      = http.createServer(app),
+    io          = require('socket.io')(server),
+    logger      = require('morgan'),
+    port        = process.env['PORT'] || 3000,
     TweetStream = require('node-tweet-stream'),
     twitter     = new TweetStream(config.twitter_api);
+
+var appDir      =  __dirname + '/../client/'; // Our NG code is served from client directory
+var environment = process.env.NODE_ENV;
+var pkg         = './../package.json';
+
 
 /**
  * Setup Express
  */
-app.use(express.static(__dirname + '/client'));
+app.use(compress({threshold: 512})); // Compress response data with gzip when byte size >= threshold
+app.use(express.static(appDir));
+app.use(favicon(appDir + 'images/favicon.ico'));
+app.use(logger('dev'));
+
+console.log('PORT=' + port);
+console.log('NODE_ENV=' + environment);
+
+
+/**
+ * Routes
+ */
 app.get('/', function (req, res) {
 
     res.redirect('/index.html')
@@ -41,6 +65,12 @@ io.on('error', function (error) {
 
 
 /**
- * Start listening op port 3000
+ * Start Serving
  */
-server.listen(3000);
+server.listen(port, function () {
+
+    console.log('Express server listening on port ' + port);
+    console.log('env = '+ app.get('env') +
+    '\n__dirname = ' + __dirname  +
+    '\nprocess.cwd = ' + process.cwd() );
+});
